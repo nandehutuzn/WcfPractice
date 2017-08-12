4faaedde60c367804df291e51540016837c5889a
nandehutuzn <120902259@qq.com>
2017-08-12 22:36:02 +0800

    start

---
+++ ZN.Core.WcfPracticeSolution/ZN.Core.WcfPractice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZN.Core.WcfPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
