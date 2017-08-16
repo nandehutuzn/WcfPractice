50f224f722510a5aca0472f160448d36474dfb09
nandehutuzn <120902259@qq.com>
2017-08-16 23:02:52 +0800

    WcfServiceLib

---
+++ ZN.Core.WcfPracticeSolution/ZN.Core.WcfServiceLib/IMyDupleService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ZN.Core.WcfServiceLib
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的接口名“IMyDupleService”。
+    [ServiceContract(CallbackContract = typeof(ICallBack))]
+    public interface IMyDupleService
+    {
+        [OperationContract]
+        void DoWork(string name);
+    }
+
+    public interface ICallBack
+    {
+        [OperationContract(IsOneWay=true)]
+        void SayHello(string mes);
+    }
+}
---
+++ ZN.Core.WcfPracticeSolution/ZN.Core.WcfServiceLib/MyDupleService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ZN.Core.WcfServiceLib
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的类名“MyDupleService”。
+    [ServiceBehavior(InstanceContextMode=InstanceContextMode.Single)]
+    public class MyDupleService : IMyDupleService, IDisposable
+    {
+        public static List<ICallBack> CallBackList { get; set; }
+
+        public MyDupleService()
+        {
+            CallBackList = new List<ICallBack>();
+        }
+
+        public void DoWork(string name)
+        {
+            ICallBack callback = OperationContext.Current.GetCallbackChannel<ICallBack>();
+            CallBackList.Add(callback);
+
+            string sessionId = OperationContext.Current.SessionId;
+            Console.WriteLine("{0} is register", sessionId);
+            OperationContext.Current.Channel.Closing += (o, e) =>
+                {
+                    lock (CallBackList)
+                    {
+                        CallBackList.Remove(callback);
+                        Console.WriteLine("{0} is remove", sessionId);
+                    }
+                };
+        }
+
+        public void Dispose()
+        {
+            CallBackList.Clear();
+        }
+    }
+}
